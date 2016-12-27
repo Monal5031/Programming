8f1f353cd5663c2cff7b8dadf504bb2ec4ad5103
Monal5031 <monalshadi16@gmail.com>
2016-12-28 02:10:51 +0530

    Added RacingHorses.py

---
+++ Competitive/CodeChef/Easy/RacingHorses.py
@@ -0,0 +1,11 @@
+T = input()
+
+for k in range(0,T):
+    N = input()
+    S = [int(x) for x in raw_input().split()]
+    S.sort()
+    min = S[1]-S[0]
+    for i in range(1,N):
+        if S[i]-S[i-1] < min:
+            min = S[i]-S[i-1]
+    print min
