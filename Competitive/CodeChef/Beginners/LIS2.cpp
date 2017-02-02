7c35fe5eb7b96aade7f358068203b89f42edbb08
Monal5031 <monalshadi16@gmail.com>
2017-02-02 15:04:32 +0530

    Added Fast Fibonacci

---
+++ Competitive/CodeChef/Beginners/LIS2.cpp
@@ -0,0 +1,21 @@
+#include"bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+ios::sync_with_stdio(false);// making i/o fast
+int tc;
+cin>>tc;
+while(tc--)
+	{
+	int n,tmp;
+	cin>>n;
+	while(n--)
+		{	
+		cin>>tmp;	
+		cout<<tmp;
+		}
+	cout<<"\n";		
+	}
+return 0;
+}
